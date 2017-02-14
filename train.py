813d5e8481d80b8bd787bbda5b77790c82e833d0
Matvey Ezhov <ematvey@ostrovok.ru>
2017-02-14 23:12:29 +0300

    train upd

--- train.py
+++ train.py
@@ -10,22 +10,15 @@ import torch.nn.functional as F
 import tasks
 from seq2seq import Seq2Seq
 
-# -- debug hook --
-import sys, ipdb, traceback
-def info(type, value, tb):
-    traceback.print_exception(type, value, tb)
-    print
-    ipdb.pm()
-sys.excepthook = info
 
-
-def train(model, task):
+def train(model, task, max_batches=1000, debug=False, verbose=False):
   batch_size = 40
 
   opt = optim.Adam(model.parameters())
 
   criterion = nn.CrossEntropyLoss()
 
+  loss_track = []
   ex_iter = tasks.preproc(task, batch_size=batch_size, eos_token=model.EOS_token)
   model.train()
   for i, (inputs, targets) in enumerate(ex_iter):
@@ -36,17 +29,34 @@ def train(model, task):
     loss.backward()
     opt.step()
 
-    if i % 100 == 0:
+    loss_track.append(loss.data[0])
+
+    if verbose and i % 100 == 0:
       _, v = logits.data.topk(1)
       inp = inputs.data.numpy().T.tolist()[0]
       fc = v.squeeze(2).numpy().T.tolist()[0]
       tg = targets.data.numpy().T.tolist()[0]
       print("%.4f\n  inputs:  %s\n  predict: %s\n  target:  %s" % (loss.data[0], inp, fc, tg))
 
-  import IPython
-  IPython.embed(banner1='train interrupted')
+    if i >= max_batches:
+      break
+
+  if debug:
+    import IPython
+    IPython.embed(banner1='train interrupted')
+
+  return loss_track
 
 if __name__ == '__main__':
+  # -- debug hook --
+  import sys, ipdb, traceback
+  def info(type, value, tb):
+      traceback.print_exception(type, value, tb)
+      print
+      ipdb.pm()
+  sys.excepthook = info
+
+
   task = tasks.ArithmeticTask()
 
   model = Seq2Seq(input_size=task.vocab_size,
